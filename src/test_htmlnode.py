4b0c36fd60f7345c177c86953a89fc243a5371f3
ausadian98 <netsub@ausadian.net>
2025-05-12 23:04:32 +0930

    switching to solution test_htmlnode.py

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -4,21 +4,51 @@ from htmlnode import HTMLNode
 
 
 class TestHTMLNode(unittest.TestCase):
-    def test_props_to_html_empty(self):
-        node = HTMLNode()
-        self.assertEqual(node.props_to_html(), "")
+    def test_to_html_props(self):
+        node = HTMLNode(
+            "div",
+            "Hello, world!",
+            None,
+            {"class": "greeting", "href": "https://boot.dev"},
+        )
+        self.assertEqual(
+            node.props_to_html(),
+            ' class="greeting" href="https://boot.dev"',
+        )
 
-    def test_props_to_html_single_prop(self):
-        node = HTMLNode(props={"href": "https://nhl.com"})
-        self.assertEqual(node.props_to_html(), ' href="https://nhl.com"')
+    def test_values(self):
+        node = HTMLNode(
+            "div",
+            "I wish I could read",
+        )
+        self.assertEqual(
+            node.tag,
+            "div",
+        )
+        self.assertEqual(
+            node.value,
+            "I wish I could read",
+        )
+        self.assertEqual(
+            node.children,
+            None,
+        )
+        self.assertEqual(
+            node.props,
+            None,
+        )
 
-    def test_props_to_html_multiple_props(self):
-        node = HTMLNode(props={"href": "https://nhl.com", "target": "_blank"})
-        result = node.props_to_html()
-        self.assertIn(' href="https://nhl.com"', result)
-        self.assertIn(' target="_blank"', result)
-        expected_length = len(' href="https://nhl.com"') + len(' target="_blank"')
-        self.assertEqual(len(result), expected_length)
+    def test_repr(self):
+        node = HTMLNode(
+            "p",
+            "What a strange world",
+            None,
+            {"class": "primary"},
+        )
+        self.assertEqual(
+            node.__repr__(),
+            "HTMLNode(p, What a strange world, children: None, {'class': 'primary'})",
+        )
 
 
 if __name__ == "__main__":
