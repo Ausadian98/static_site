c57cc84f2163c74f94d6665bb64bef64d8a02a1f
ausadian98 <netsub@ausadian.net>
2025-05-29 22:46:38 +0930

    L6 Text to TextNodes

--- src/test_split_delimiter.py
+++ src/test_split_delimiter.py
@@ -1,6 +1,6 @@
 import unittest
 
-from split_delimiter import split_nodes_delimiter
+from split_delimiter import split_nodes_delimiter, text_to_textnodes
 from textnode import TextNode, TextType
 
 
@@ -83,3 +83,23 @@ class TestSplitNodesDelimiter(unittest.TestCase):
         self.assertEqual(result[1].text, "Bold at start")
         self.assertEqual(result[2].text, " and ")
         self.assertEqual(result[3].text, "bold at end")
+
+    def test_text_to_textnodes(self):
+        nodes = text_to_textnodes(
+            "This is **text** with an _italic_ word and a `code block` and an ![image](https://i.imgur.com/zjjcJKZ.png) and a [link](https://boot.dev)"
+        )
+        self.assertListEqual(
+            [
+                TextNode("This is ", TextType.TEXT),
+                TextNode("text", TextType.BOLD),
+                TextNode(" with an ", TextType.TEXT),
+                TextNode("italic", TextType.ITALIC),
+                TextNode(" word and a ", TextType.TEXT),
+                TextNode("code block", TextType.CODE),
+                TextNode(" and an ", TextType.TEXT),
+                TextNode("image", TextType.IMAGE, "https://i.imgur.com/zjjcJKZ.png"),
+                TextNode(" and a ", TextType.TEXT),
+                TextNode("link", TextType.LINK, "https://boot.dev"),
+            ],
+            nodes,
+        )
